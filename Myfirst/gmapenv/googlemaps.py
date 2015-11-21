3aeffcd44c97e6d06d316f8f295cad677313d396
mamdouh <egycan27@yahoo.com>
2015-11-21 02:41:35 -0800

    initial commit

---
+++ Myfirst/gmapenv/googlemaps.py
@@ -0,0 +1,3 @@
+from googlemaps import GoogleMaps
+address = '207 N. Defiance St, Archbold, OH'
+print googlemaps.address(address)
---
+++ Myfirst/myfirst.py
@@ -0,0 +1,7 @@
+__author__ = 'mamdouh'
+
+import Tkinter
+import os
+import sys
+print "hello"
+print (os.path.dirname(os.path.abspath(__file__)))
