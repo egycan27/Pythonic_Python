7011dc81357ebc46e4ee3cf2a1f6d2a5d424f202
egycan27 <egycan27@yahoo.com>
2015-11-22 09:44:03 -0800

    New

--- xml_web.py
+++ xml_web.py
@@ -1,9 +1,16 @@
 import itertools
+#Creating a max numbers of errors
+# current number of concecutive dowload errors
+num_errors = 0
 for page in itertools.count(1):
 	url = 'http:://example.webscraping.com/view/-%d' % page
 	html = download(url)
 	if html = None :
-		breake
+		num_errors +=1
+		if num_errors == max_errors:
+
+			breake
 	else:
 		# success - can scrap the site
 		pass
+		num_errors = 0
