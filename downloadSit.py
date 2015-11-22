c7ad974863d7a26999b193d7716f00716cab311d
egycan27 <egycan27@yahoo.com>
2015-11-21 22:20:49 -0800

    web crawling script

---
+++ downloadSit.py
@@ -0,0 +1,13 @@
+import urllib2
+
+def download(url):
+    print 'Downloading:', url
+    try:
+        html = urllib2.urlopen(url).read()
+    except urllib2.URLError as e:
+        print 'Download error:', e.reason
+        html = None
+    print html
+if __name__ == '__main__':
+    download('https://www.vancity.com')
+
